48dac3f4a7811982cf33dd92144752b8f533fb96
unknown <fpgeoghegan@gmail.com>
2021-04-17 17:24:09 -0300

    finish homework I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,9 +60,9 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (x === y) return true
+  if (x === y) return true;
   else
-  return false
+  return false;
 }
 
 function tienenMismaLongitud(str1, str2) {
